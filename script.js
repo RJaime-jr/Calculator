f4dc007a4f73c65840d81b4bf401121505e870c4
RJaime-jr <Ricoa.jaime@outlook.com>
2022-06-26 15:33:38 -0700

    Created a lot of variables, Worked on buttons and displays, just need to work on operations and returning the final value

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+//Setting up Variables for Selectors
+//Buttons
 let numButtons = document.querySelectorAll('.num-btn');
+let symButtons = document.querySelectorAll('.sym');
+let clear = document.querySelector('.clear');
+let del = document.querySelector('.del');
+let div = document.querySelector('.div');
+let mltply = document.querySelector('.mltply');
+let minus = document.querySelector('.minus');
+let plus = document.querySelector('.pls');
+let equals = document.querySelector('.equ');
+
+//Display
+let primaryDisplay = document.getElementById("current-val");
+let secondaryDisplay = document.getElementById("previous-val");
+let oppDisplay = document.getElementById("operation");
+
+//Empty Variables
 let currentVal = [];
-let firstVal = '';
-let primaryDisplay = document.getElementsByClassName('current-val').innerHtml;
+let SecondVal = 0;
+let currentOpperand = '';
+let result = 0;
+
+//Display Functions & Inputs
 
 function currentValPrint(num){
+  if(currentVal.length > 7){currentVal.pop();}
     currentVal.push(num);
     let numString = currentVal.join("");
     return parseInt(numString);
@@ -11,21 +32,103 @@ function currentValPrint(num){
 
 numButtons.forEach((button) =>
   button.addEventListener('click', function(){
-   
-   
-    primaryDisplay = `${button.textContent}`;
-   
-    console.log(primaryDisplay);
+    primaryDisplay.innerText = currentValPrint(`${button.textContent}`);
+    console.log(primaryDisplay.textContent);
     return;
   }));
  
+// Clear Button 
+clear.addEventListener('click', function(){
+  currentVal = [];
+  secondVal = '';
+  currentOpperand = '';
+  primaryDisplay.innerText = '0';
+  secondaryDisplay.innerText = '';
+  oppDisplay.innerText = '';
+  return;
+})
+
+// Delete Button
+del.addEventListener('click', function(){
+    currentVal.pop();
+    let numString = currentVal.join("");
+  primaryDisplay.innerText = parseInt(numString);
+  return;
+  
+})
+//Getting Opperand
+symButtons.forEach((button) =>
+button.addEventListener('click', function(){
+  SecondVal = parseInt(currentVal.join(""));
+  currentVal = [];
+  secondaryDisplay.innerText = SecondVal;
+  oppDisplay.innerText = `${button.textContent}`;
+  primaryDisplay.innerText = "";
+  
+  currentOpperand = button.textContent;
+  console.log(currentOpperand);
+  return;
+
+}));
+
+//Individual Symbol button events
 
 
 
-/*numButtons.addEventListener('click',function(e){
-//currentValPrint(e);
-console.log("hello");
-document.getElementsByClassName('current-val').innerText = currentValPrint(e);
-return
+//Divide
+div.addEventListener('click', function(){
+ 
+
 });
-*/
+
+//Multiply 
+
+mltply.addEventListener('click', function(){
+
+ 
+});
+
+//Subtract
+minus.addEventListener('click', function(){
+  
+
+});
+
+//Add
+plus.addEventListener('click', function(){
+  if(SecondVal != "" && parseInt(currentVal.join(""))!= ""){
+    
+    result = add(parseInt(currentVal.join("")),SecondVal);
+  }
+  console.log(result);
+  return;
+});
+
+
+//Equals
+equals.addEventListener('click', function(){
+  if(SecondVal != "" && parseInt(currentVal.join(""))!= ""){
+    
+    primaryDisplay.innerText = result;
+  }
+});
+
+  // Calculator Functions
+
+  //Multiply 
+  function multiply(a,b){
+    return a * b;
+  }
+
+  function divide(a,b){
+    return a/b;
+  }
+
+
+  function add(a,b){
+    return a + b;
+  }
+
+  function subtract(a,b){
+    return a - b;
+  }
